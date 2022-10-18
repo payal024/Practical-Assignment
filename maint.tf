d3cf1055f4f6c642721e2a91d53c3fb8500f93ef
payal024 <65292933+payal024@users.noreply.github.com>
2022-10-18 18:57:59 +0530

    Update maint.tf

--- maint.tf
+++ maint.tf
@@ -245,18 +245,18 @@ ingress {
 # Generate the SSH keypair that well use to configure the EC2 instance.
 # After that, write the private key to a local file and upload the public key to AWS
 
-resource "tls_private_key" "key" {
-  algorithm = "RSA"
-}
-resource "local_file" "private_key" {
-  filename          = "TEST.pem"
- #sensitive_content = tls_private_key.key.private_key_pem
-  file_permission   = "0400"
-}
-resource "aws_key_pair" "key_pair" {
-  key_name   = "TEST"
-  public_key = tls_private_key.key.public_key_openssh
-}
+# resource "tls_private_key" "key" {
+#   algorithm = "RSA"
+# }
+# resource "local_file" "private_key" {
+#   filename          = "TEST.pem"
+#  #sensitive_content = tls_private_key.key.private_key_pem
+#   file_permission   = "0400"
+# }
+# resource "aws_key_pair" "key_pair" {
+#   key_name   = "TEST"
+#   public_key = tls_private_key.key.public_key_openssh
+# }
 
 
 # Create a new EC2 launch configuration
