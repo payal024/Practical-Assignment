be3de605a00fe7803eef0fafcb262ed0f88d045e
payal024 <65292933+payal024@users.noreply.github.com>
2022-10-18 19:04:09 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,10 +1,10 @@
-output "ssh_keypair" {
-value = tls_private_key.key.private_key_pem
-sensitive = true
-}
-output "key_name" {
-value = aws_key_pair.key_pair.key_name
-}
+# output "ssh_keypair" {
+# value = tls_private_key.key.private_key_pem
+# sensitive = true
+# }
+# output "key_name" {
+# value = aws_key_pair.key_pair.key_name
+# }
 output "public_ip" {
 value = aws_instance.ec2_public.public_ip
 }
